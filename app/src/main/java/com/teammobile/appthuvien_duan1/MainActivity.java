740472a3cb87add6e510e757441d7e5077c47905
anhaylientuc <thang3051998no1@gmail.com>
2023-07-08 17:18:52 +0700

    Bắt sự kiện button

--- app/src/main/java/com/teammobile/appthuvien_duan1/MainActivity.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/MainActivity.java
@@ -3,12 +3,22 @@ package com.teammobile.appthuvien_duan1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"ok",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
