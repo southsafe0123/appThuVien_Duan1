ecd2eef4dbd1b390e015acc4b3d784f07b066a54
anhaylientuc <thang3051998no1@gmail.com>
2023-07-09 17:04:10 +0700

    e21e
    31

--- app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.teammobile.appthuvien_duan1;
+package com.teammobile.appthuvien_duan1.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.teammobile.appthuvien_duan1.R;
 
 public class MainActivity extends AppCompatActivity {
 
