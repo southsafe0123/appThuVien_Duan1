e36abd6a0a716fbae4c01aedcfb417f7f44309d8
anhaylientuc <thang3051998no1@gmail.com>
2023-07-09 14:45:42 +0700

    vdzfv

--- app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
@@ -1,23 +1,59 @@
 package com.teammobile.appthuvien_duan1.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.teammobile.appthuvien_duan1.R;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bnvMain;
+    NavigationBarView bnvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bnvMain=findViewById(R.id.bnvMain);
+        bnvMain.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+
+                switch (item.getItemId()){
+                    case R.id.mnHome:
+                    {
+                        Toast.makeText(MainActivity.this,"Bạn chọn Home",Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.mnSearch:
+                    {
+                        Toast.makeText(MainActivity.this,"Bạn chọn Search",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this,SearchActivity.class));
+                        break;
+                    }
+                    case R.id.mnCart:
+                    {
+                        Toast.makeText(MainActivity.this,"Bạn chọn Cart",Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.mnUser:
+                    {
+                        Toast.makeText(MainActivity.this,"Bạn chọn User",Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/teammobile/appthuvien_duan1/activity/SearchActivity.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/activity/SearchActivity.java
@@ -2,7 +2,14 @@ package com.teammobile.appthuvien_duan1.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.teammobile.appthuvien_duan1.R;
 
@@ -12,5 +19,32 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        handleIntent(getIntent());
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(SearchActivity.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(SearchActivity.this, "OK", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+
+
 }
