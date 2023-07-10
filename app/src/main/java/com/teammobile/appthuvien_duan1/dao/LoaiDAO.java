ec9c8f702bf4cf616f559560fef9b1add0e79bd1
anhaylientuc <thang3051998no1@gmail.com>
2023-07-11 02:10:25 +0700

    Dda
    dDd

--- app/src/main/java/com/teammobile/appthuvien_duan1/dao/LoaiDAO.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/dao/LoaiDAO.java
@@ -1,7 +1,13 @@
 package com.teammobile.appthuvien_duan1.dao;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.teammobile.appthuvien_duan1.interfaces.ILoai;
+import com.teammobile.appthuvien_duan1.model.LoaiSach;
 
 public class LoaiDAO {
     private FirebaseDatabase mDatabase;
@@ -9,5 +15,20 @@ public class LoaiDAO {
     public LoaiDAO(){
         mDatabase=FirebaseDatabase.getInstance();
         reference=mDatabase.getReference().child("Loai");
+    }
+    public void insert(LoaiSach loaiSach, ILoai iLoai)
+    {
+        reference.push().setValue(loaiSach).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                iLoai.onCallBack(true);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                iLoai.onCallBack(false);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/teammobile/appthuvien_duan1/interfaces/ILoai.java
@@ -0,0 +1,5 @@
+package com.teammobile.appthuvien_duan1.interfaces;
+
+public interface ILoai {
+    public void onCallBack(Boolean check);
+}
