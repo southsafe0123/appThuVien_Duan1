4743b89f3b33a0c78573ba2f9bf5850c7bace6a3
southsafe0123 <naman.nguyen49002@gmail.com>
2023-07-13 15:50:08 +0700

    test

--- app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        djaokfjdois
     }
 }
