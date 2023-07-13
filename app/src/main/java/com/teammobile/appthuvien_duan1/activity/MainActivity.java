6d5fd144093e46310015f6a520de8f74703f1ee2
southsafe0123 <naman.nguyen49002@gmail.com>
2023-07-13 16:05:16 +0700

    Update MainActivity.java

--- app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
@@ -13,6 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
