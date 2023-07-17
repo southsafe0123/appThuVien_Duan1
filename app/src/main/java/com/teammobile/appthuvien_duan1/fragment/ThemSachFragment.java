78e82491330ff0d2fba76db1b5da4292c89a508c
anhaylientuc <thang3051998no1@gmail.com>
2023-07-17 11:27:50 +0700

    qua met moi

---
+++ app/src/main/java/com/teammobile/appthuvien_duan1/fragment/ThemSachFragment.java
@@ -0,0 +1,24 @@
+package com.teammobile.appthuvien_duan1.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.teammobile.appthuvien_duan1.R;
+
+public class ThemSachFragment extends Fragment {
+    Context context;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        context=getContext();
+        View view=LayoutInflater.from(context).inflate(R.layout.fragment_them_sach,container,false);
+        return view;
+    }
+}
