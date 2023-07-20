dd7395ed14fb3436a9b234af2a6cf50af7e53ee6
An-Panda2010 <dangbinhan2505@gmail.com>
2023-07-20 15:16:18 +0700

    update fragment

--- app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/activity/MainActivity.java
@@ -1,16 +1,23 @@
 package com.teammobile.appthuvien_duan1.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import com.teammobile.appthuvien_duan1.R;
+import com.teammobile.appthuvien_duan1.fragment.FragmentHome;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentHome fragmentHome = new FragmentHome();
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().add(R.id.viewFragment, fragmentHome).commit();
     }
 }
