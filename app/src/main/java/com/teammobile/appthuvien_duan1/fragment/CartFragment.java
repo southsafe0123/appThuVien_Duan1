a39e0ac70a39ea21b85f0f52d0424a5e4c5f478d
anhaylientuc <thang3051998no1@gmail.com>
2023-07-25 13:16:18 +0700

    Update CartFragment.java

--- app/src/main/java/com/teammobile/appthuvien_duan1/fragment/CartFragment.java
+++ app/src/main/java/com/teammobile/appthuvien_duan1/fragment/CartFragment.java
@@ -19,7 +19,6 @@ public class CartFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
         recyclerView = view.findViewById(R.id.RvCart);
-
         return view;
     }
 }
